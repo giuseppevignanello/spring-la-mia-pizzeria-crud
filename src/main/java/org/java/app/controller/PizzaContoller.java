94612f59369163e082ef83f42f5a9b181d23bae2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-10-05 11:41:07 +0200

    add show method

--- src/main/java/org/java/app/controller/PizzaContoller.java
+++ src/main/java/org/java/app/controller/PizzaContoller.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,6 +25,14 @@ public class PizzaContoller {
 		List<Pizza> pizzas = pizzaService.findAll();
 		model.addAttribute("pizzas", pizzas);
 		return "index";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(Model model, @PathVariable int id) {
+		
+		Pizza pizza = pizzaService.findById(id);
+		model.addAttribute("pizza", pizza);	
+		return "show";
 	}
 
 }
