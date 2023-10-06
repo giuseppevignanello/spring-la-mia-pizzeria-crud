2b87405f3176f2e9fe8fd2c9d5fd749089dd1e23
giuseppevignanello <peppe.vignanello@gmail.com>
2023-10-06 12:00:07 +0200

    add store method

--- src/main/java/org/java/app/controller/PizzaContoller.java
+++ src/main/java/org/java/app/controller/PizzaContoller.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,6 +37,7 @@ public class PizzaContoller {
 		model.addAttribute("pizza", pizza);	
 		return "show";
 	}
+	
 	@GetMapping("/create")
 	public String getCreateForm(Model model) {
 		
@@ -42,5 +45,12 @@ public class PizzaContoller {
 		
 		return "create";
 	}
+	
+	@PostMapping("/create")
+	public String store(@ModelAttribute Pizza pizza, Model model) {
+		pizzaService.save(pizza);
+		return "create";
+	}
+	
 
 }
